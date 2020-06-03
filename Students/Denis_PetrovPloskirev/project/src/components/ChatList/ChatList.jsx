9cfad648e7951811d5a7722a885ad511744b6fab
Denis <todenis@inbox.ru>
2020-06-03 22:47:22 +0300

    Denis Petrov-Ploskirev. Hometask#6

--- Students/Denis_PetrovPloskirev/project/src/components/ChatList/ChatList.jsx
+++ Students/Denis_PetrovPloskirev/project/src/components/ChatList/ChatList.jsx
@@ -3,17 +3,12 @@ import React from 'react';
 import './ChatList.css';
 
 import { Link } from 'react-router-dom';
-import Avatar from 'material-ui/svg-icons/social/sentiment-satisfied';
-import AvatarBad from 'material-ui/svg-icons/social/sentiment-dissatisfied';
-import AvatarThree from 'material-ui/svg-icons/social//sentiment-very-dissatisfied';
-import AvatarFour from 'material-ui/svg-icons/social/sentiment-very-satisfied';
 import DeleteIcon from 'material-ui/svg-icons/navigation/cancel';
-import {TextField} from 'material-ui';
+import { TextField } from 'material-ui';
 import AddIcon from 'material-ui/svg-icons/content/add';
 
 import { connect } from 'react-redux';
-import { addChat } from '../../store/actions/chats_actions.js';
-import { deleteChat } from '../../store/actions/chats_actions.js';
+import { addChat, deleteChat, loadChats } from '../../store/actions/chats_actions.js';
 import { bindActionCreators } from 'redux';
 
 class ChatList extends React.Component {
@@ -23,63 +18,66 @@ class ChatList extends React.Component {
 
   handleAdd = () => {
     if (this.state.input) {
-        this.props.addChat(this.state.input);
-        this.setState({input: ''});
+      this.props.addChat(this.state.input);
+      this.setState({ input: '' });
     }
-}
+  }
 
-handleChange = (evt) => {
+  handleChange = (evt) => {
     if (evt.keyCode !== 13) this.setState({ [evt.target.name]: evt.target.value });
-}
+  }
 
-handleKeyUp = evt => {
+  handleKeyUp = evt => {
     if (evt.keyCode == 13) this.handleAdd();
-}
+  }
 
-deleteChat = (e) => {
-  e.stopPropagation();
-  e.preventDefault();
-  let id = e.nativeEvent.target.dataset.id ? e.nativeEvent.target.dataset.id : e.nativeEvent.target.parentElement.dataset.id
-  this.props.deleteChat(id);
-}
+  deleteChat = (id, e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    this.props.deleteChat(id);
+  }
+
+  componentDidMount() {
+    this.props.loadChats();
+  }
 
   render() {
     let { chats } = this.props;
     return (
-      <div className = "chatList">
-        <div className = "chatListInner">
-          { Object.keys(chats).map((key) => (
-            <Link to = { `/chat/${key}/` } key = { key }>
-              <div className = { this.props.active == chats[key].title ? 'chatListItem chatListItemActive' : 'chatListItem' }>
-                  { `  ${chats[key].title}` }
-                <span className="deleteIcon">
-                  <DeleteIcon data-id = { key } onClick = {this.deleteChat} viewBox="0 0 24 24" style = { {"width": "12px", "height": "12px"} } color = "antiquewhite" hoverColor = "darkred" />
-                </span>
-              </div>
-            </Link>
-          )) }
-        </div>
-        <div className = "chatAdder">
-          <TextField 
-            key = "textField"
-            name = "input"
-            hintText = "Add new chat"
-            underlineFocusStyle = { {borderColor: 'darkgoldenrod'} }
-            style = { {fontSize: '12px', width: "calc(100% - 34px)", marginLeft: "10px"} }
-            inputStyle = { {color: "dimgray"} }
-            onChange = { this.handleChange }
-            value = { this.state.input }
-            onKeyUp = { this.handleKeyUp }
-          >
-          
-          </TextField>
-          <span onClick = { this.handleAdd }>
-            <AddIcon 
-              color = "grey"
-              hoverColor = "darkgoldenrod"
-              style = { {cursor: "pointer", width: "24px", height: "24px"} }
+      <div className="chatList">
+        <div className="chatListInner">
+          {Object.keys(chats).map((key) => (
+            <Link to={`/chat/${key}/`} key={key} id={key}
+              children={
+                <div className={this.props.active == chats[key].title ? 'chatListItem chatListItemActive' : 'chatListItem'}>
+                  {`  ${chats[key].title}`}
+                  <span className="deleteIcon">
+                    <DeleteIcon onClick={this.deleteChat.bind(this, key)} viewBox="0 0 24 24" style={{ "width": "12px", "height": "12px" }} color="antiquewhite" hoverColor="darkred" />
+                  </span>
+                </div>
+              }
             />
-          </span>
+          ))}
+        </div>
+        <div className="chatAdder">
+          <TextField
+            key="textField"
+            name="input"
+            hintText="Add new chat"
+            underlineFocusStyle={{ borderColor: 'darkgoldenrod' }}
+            style={{ fontSize: '12px', width: "calc(100% - 34px)", marginLeft: "10px" }}
+            inputStyle={{ color: "dimgray" }}
+            onChange={this.handleChange}
+            value={this.state.input}
+            onKeyUp={this.handleKeyUp}
+          >
+          </TextField>
+          <AddIcon
+            onClick={this.handleAdd}
+            color={!this.state.input ? "rgb(243, 243, 243)" : "grey"}
+            hoverColor={!this.state.input ? "rgb(243, 243, 243)" : "darkgoldenrod"}
+            style={{ cursor: "pointer", width: "24px", height: "24px" }}
+          />
         </div>
       </div>
     )
@@ -88,6 +86,6 @@ deleteChat = (e) => {
 
 const mapStateToProps = ({ chatsReducer }) => ({ chats: chatsReducer.chats });
 
-const mapDispatchToProps = dispatch => bindActionCreators({ addChat, deleteChat }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ addChat, deleteChat, loadChats }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
