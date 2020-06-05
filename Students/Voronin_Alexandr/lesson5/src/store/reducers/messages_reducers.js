eadcfd521f816a1036bc43fedd1e455adcbd5c2a
sanchezraven <sanchez_raven@mail.ru>
2020-06-06 01:21:54 +0500

    hw7

--- Students/Voronin_Alexandr/lesson5/src/store/reducers/messages_reducers.js
+++ Students/Voronin_Alexandr/lesson5/src/store/reducers/messages_reducers.js
@@ -1,19 +1,26 @@
 import update from 'react-addons-update';
 //import actions(methods)
-import { SEND_MSG, SUCCESS_MESSAGES_LOADING } from '../actions/messages_actions.js'
+import { SUCCESS_MESSAGES_LOADING } from '../actions/messages_actions.js'
+import { SUCCESS_MESSAGE_SEND } from '../actions/messages_actions.js'
 
 
 let action;         //{type: 'some type'}
 const initialStore = {
     messages: {}
+    // messages: []
 }
 
 export default function msgReducer(store = initialStore, action) {
     switch(action.type) {
-        case SEND_MSG: {
-            return update(store, {      //update - same Object.assign    
-                messages: {$merge: { [action.messageId]: {user: action.sender, text: action.text} }}                //some act with reducer
-            })                               
+        case SUCCESS_MESSAGE_SEND: {
+            if (action.payload.response.status) {
+                return update(store, {      //update - same Object.assign    
+                    messages: {$merge: { [action.payload.msg.messageId]: {user: action.payload.msg.sender, text: action.payload.msg.text} }}                //some act with reducer
+                })
+            } else {
+                console.log('Error send msg', action.payload);
+                return null
+            }                             
         }
         case SUCCESS_MESSAGES_LOADING: {
             return update(store, {
--- Students/Voronin_Alexandr/lesson5/webpack.config.js
+++ Students/Voronin_Alexandr/lesson5/webpack.config.js
@@ -5,6 +5,18 @@ const path = require ('path');
 
 module.exports = {
     devServer: {
+        contentBase: './dist',
+        port: 3000,
+        hot: true,
+        open: false,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3300',
+                pathRewrite: { '^/api': '' },
+                secure: false,
+                changeOrigin: true
+            }
+        },
         historyApiFallback: true
     },
     entry: {
