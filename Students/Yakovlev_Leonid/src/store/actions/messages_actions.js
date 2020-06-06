637d87542b7d566599b120e683abaf04f6c5e389
Yakovlev Leonid <amazing.leo@yandex.ru>
2020-06-06 16:10:14 +0500

    lesson 6

--- Students/Yakovlev_Leonid/src/store/actions/messages_actions.js
+++ Students/Yakovlev_Leonid/src/store/actions/messages_actions.js
@@ -1,4 +1,11 @@
+import { getJSON, RSAA } from "redux-api-middleware";
+
 export let SEND_MSG = '@@messages/SEND';
+
+export let START_MESSAGES_LOADING = '@@messages/START_MESSAGES_LOADING';
+export let SUCCES_MESSAGES_LOADING = '@@messages/SUCCES_MESSAGES_LOADING';
+export let ERROR_MESSAGES_LOADING = '@@messages/ERROR_MESSAGES_LOADING';
+
 
 export let sendMessage = (messageId, sender, text) => ({
     type: SEND_MSG,
@@ -6,3 +13,19 @@ export let sendMessage = (messageId, sender, text) => ({
     sender,
     text
 });
+
+export const loadMessages = () => ({
+    [RSAA]: {
+        endpoint: './server/db/messages.json',
+        method: 'GET',
+        types: [
+            START_MESSAGES_LOADING,
+            {
+                type: SUCCES_MESSAGES_LOADING,
+                payload: (action, state, res) => getJSON(res)
+                            .then(json => json)
+            },
+            ERROR_MESSAGES_LOADING
+        ]
+    }
+})
--- Students/Yakovlev_Leonid/src/store/store.js
+++ Students/Yakovlev_Leonid/src/store/store.js
@@ -1,8 +1,29 @@
 import initialReducers from './reducers';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import middlewares from '../components/middlewares/index.js'
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
 
-export default function initStore() {
+export const history = createBrowserHistory();
+
+export function initStore() {
     let initialStore = {};
 
-    return createStore(initialReducers, initialStore)
+    // return createStore(initialReducers, initialStore)
+    // return createStore(
+    //     initialReducers,
+    //     initialStore,
+    //     compose(
+    //         applyMiddleware(...middlewares)
+    //     )
+    // );
+
+    return createStore(
+        initialReducers(history),
+        initialStore,
+        compose(
+            applyMiddleware(routerMiddleware(history), ...middlewares)
+            // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => {},
+        )
+    );
 }
